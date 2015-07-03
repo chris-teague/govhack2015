6ca1d060b62ade8a1790d8b219a8c519d8898b36
Chris Teague <chris@cteague.com.au>
2015-07-04 02:30:30 +0930

    Adding neighbouring area code to postcodes & electorates

--- app/models/electorate.rb
+++ app/models/electorate.rb
@@ -10,4 +10,19 @@ class Electorate < ActiveRecord::Base
     url
   end
 
+  def neigbouring
+    Electorate.find_by_sql(
+      "SELECT e.*
+       FROM   electorates e
+       WHERE  e.id != #{id} AND
+              ST_Intersects(ST_Expand((SELECT area FROM electorates WHERE id = #{id}), 0.02), e.area)")
+  end
+
+  def neigbouring_postcodes
+    Postcode.find_by_sql(
+      "SELECT p.*
+       FROM   postcodes p
+       WHERE  ST_Intersects(ST_Expand((SELECT area FROM electorates WHERE id = #{id}), 0.02), p.area)")
+  end
+
 end
--- app/models/postcode.rb
+++ app/models/postcode.rb
@@ -10,4 +10,19 @@ class Postcode < ActiveRecord::Base
     url
   end
 
+  def neigbouring
+    Postcode.find_by_sql(
+      "SELECT p.*
+       FROM   postcodes p
+       WHERE  p.id != #{id} AND
+              ST_Contains(ST_Expand((SELECT area FROM postcodes WHERE id = #{id}), 0.02), p.area)")
+  end
+
+  def neigbouring_electorates
+    Electorate.find_by_sql(
+      "SELECT e.*
+       FROM   electorates e
+       WHERE  ST_Intersects(ST_Expand((SELECT area FROM postcodes WHERE id = #{id}), 0.02), e.area)")
+  end
+
 end
