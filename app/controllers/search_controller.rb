983079b7796d44d4f5f0a80d5c88f5a81076140c
Chris Teague <chris@cteague.com.au>
2015-07-04 01:22:55 +0930

    Adding relocation search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,4 +5,15 @@ class SearchController < ApplicationController
     results = (postcodes | electorates).map { |t| { value: t.name, id: "#{t.class}:#{t.id}" } }
     render text: results.to_json
   end
+
+  def relocate
+    if postcode = Postcode.where(name: params[:location]).first
+      redirect_to postcode_url(postcode.id)
+    elsif electorate = Electorate.where(name: params[:location]).first
+      redirect_to electorate_url(electorate.id)
+    else
+      flash[:notice] = "Can't find your location, yo"
+      redirect_to '/'
+    end
+  end
 end
