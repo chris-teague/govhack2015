9e9809f006e7648bbee9d4c3448c323e9173fa1e
Chris Teague <chris@cteague.com.au>
2015-07-05 15:48:38 +0930

    Caching & final prep

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def locations
     postcodes = Postcode.select([:name, :id]).where('name ILIKE ?', "%#{params[:term]}%").limit(100).all
     electorates = Electorate.select([:name, :id]).where('name ILIKE ?', "%#{params[:term]}%").limit(100).all
-    results = (postcodes | electorates).map { |t| { value: t.name, id: "#{t.class}:#{t.id}" } }
+    results = (electorates | postcodes).map { |t| { value: t.name, id: "#{t.class}:#{t.id}" } }
     render text: results.to_json
   end
 
